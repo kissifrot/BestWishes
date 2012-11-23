dd11d1a10c8aaf9be989af8a100596b8886c183e
Philippe Villiers <kissifrot@gmail.com>
2012-11-23 14:39:38 +0100

    Bump version number.

--- lib/BwVersion.class.php
+++ lib/BwVersion.class.php
@@ -1,11 +1,15 @@
 <?php
 final class BwVersion
 {
-	const VERSION = '0.5';
+	const VERSION = '0.6';
 
 	protected static $_lastestVersion;
 
+	/**
+	 * @TODO : Check the latest version on BW's website and return it
+	 */
 	public static function getLatest()
 	{
+		return true;
 	}
 }
