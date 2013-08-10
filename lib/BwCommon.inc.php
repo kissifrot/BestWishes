80ea6d9c2e197a31b405a105a9bb1cf86a371c82
Philippe Villiers <kissifrot@gmail.com>
2013-08-10 19:39:43 +0200

    Add check for existing boolval() function (as it's shipped with PHP 5.5)

--- lib/BwCommon.inc.php
+++ lib/BwCommon.inc.php
@@ -104,35 +104,37 @@ function boolString($bValue = false) {
 	return ($bValue ? 'true' : 'false');
 }
 
-/** 
- * Borrowed from http://php.net/manual/en/function.is-bool.php#88635
- * Checks a variable to see if it should be considered a boolean true or false.
- *     Also takes into account some text-based representations of true of false,
- *     such as 'false','N','yes','on','off', etc.
- * @author Samuel Levy <sam+nospam@samuellevy.com>
- * @param mixed $in The variable to check
- * @param bool $strict If set to false, consider everything that is not false to
- *                     be true.
- * @return bool The boolean equivalent or null
- */
-function boolVal($in, $strict = false) {
-	$out = null;
-	// if not strict, we only have to check if something is false
-	if (in_array($in,array('false', 'False', 'FALSE', 'no', 'No', 'n', 'N', '0', 'off',
-						'Off', 'OFF', false, 0, null), true)) {
-		$out = false;
-	} else if ($strict) {
-		// if strict, check the equivalent true values
-		if (in_array($in,array('true', 'True', 'TRUE', 'yes', 'Yes', 'y', 'Y', '1',
-							'on', 'On', 'ON', true, 1), true)) {
-			$out = true;
+if(!function_exists('boolval')) {
+	/** 
+	 * Borrowed from http://php.net/manual/en/function.is-bool.php#88635
+	 * Checks a variable to see if it should be considered a boolean true or false.
+	 *     Also takes into account some text-based representations of true of false,
+	 *     such as 'false','N','yes','on','off', etc.
+	 * @author Samuel Levy <sam+nospam@samuellevy.com>
+	 * @param mixed $in The variable to check
+	 * @param bool $strict If set to false, consider everything that is not false to
+	 *                     be true.
+	 * @return bool The boolean equivalent or null
+	 */
+	function boolVal($in, $strict = false) {
+		$out = null;
+		// if not strict, we only have to check if something is false
+		if (in_array($in,array('false', 'False', 'FALSE', 'no', 'No', 'n', 'N', '0', 'off',
+							'Off', 'OFF', false, 0, null), true)) {
+			$out = false;
+		} else if ($strict) {
+			// if strict, check the equivalent true values
+			if (in_array($in,array('true', 'True', 'TRUE', 'yes', 'Yes', 'y', 'Y', '1',
+								'on', 'On', 'ON', true, 1), true)) {
+				$out = true;
+			}
+		} else {
+			// not strict? let the regular php bool check figure it out (will
+			//     largely default to true)
+			$out = ($in?true:false);
 		}
-	} else {
-		// not strict? let the regular php bool check figure it out (will
-		//     largely default to true)
-		$out = ($in?true:false);
+		return $out;
 	}
-    return $out;
 }
 
 /**
