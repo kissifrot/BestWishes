dce38053c4ed1da94ceed3188a5a95223609e217
Philippe Villiers <kissifrot@gmail.com>
2017-09-26 17:26:57 +0200

    Packages update. PHP 7.1+ is now mandatory

--- web/app.php
+++ web/app.php
@@ -2,10 +2,7 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
-/**
- * @var Composer\Autoload\ClassLoader
- */
-$loader = require __DIR__.'/../app/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 if (PHP_VERSION_ID < 70000) {
     include_once __DIR__.'/../var/bootstrap.php.cache';
 }
