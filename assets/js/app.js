04550d2bff535d978e0c48d2071284999c779b68
kissifrot <kissifrot+gh@gmail.com>
2022-11-05 22:14:20 +0100

    Slight adjustments in gifts display

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,5 +3,9 @@ require('../scss/app.scss');
 const $ = require('jquery');
 global.$ = global.jQuery = $;
 
-require('bootstrap');
+const bootstrap = require('bootstrap');
 require('bootstrap/js/dist/modal');
+require('bootstrap/js/dist/tooltip');
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
