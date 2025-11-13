e14077d8267da4302ffabaa7fc85054382c964ac
kissifrot <kissifrot@gmail.com>
2025-11-13 22:25:23 +0100

    chore: fix non-working legacy migrations

--- migrations/Version20200423202024.php
+++ migrations/Version20200423202024.php
@@ -9,14 +9,17 @@ use Doctrine\Migrations\AbstractMigration;
 
 final class Version20200423202024 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return 'Drop FOSUserBundle and adjust User entity';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
-        $this->skipIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->skipIf(
+            $schema->hasTable('bw_reset_password_request'),
+            'Table bw_reset_password_request already exists, skipping migration'
+        );
 
         $this->addSql('CREATE TABLE bw_reset_password_request (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', expires_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_398C30ECA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE bw_reset_password_request ADD CONSTRAINT FK_398C30ECA76ED395 FOREIGN KEY (user_id) REFERENCES bw_user (id)');
@@ -27,10 +30,8 @@ final class Version20200423202024 extends AbstractMigration
         $this->addSql('CREATE UNIQUE INDEX UNIQ_81F6EE84F85E0677 ON bw_user (username)');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
-        $this->skipIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
         $this->addSql('DROP TABLE bw_reset_password_request');
         $this->addSql('DROP INDEX UNIQ_81F6EE84F85E0677 ON bw_user');
         $this->addSql('ALTER TABLE bw_user ADD username_canonical VARCHAR(180) CHARACTER SET utf8 NOT NULL COLLATE `utf8_unicode_ci`, ADD email_canonical VARCHAR(180) CHARACTER SET utf8 NOT NULL COLLATE `utf8_unicode_ci`, ADD enabled TINYINT(1) NOT NULL, ADD salt VARCHAR(255) CHARACTER SET utf8 DEFAULT \'NULL\' COLLATE `utf8_unicode_ci`, ADD confirmation_token VARCHAR(180) CHARACTER SET utf8 DEFAULT \'NULL\' COLLATE `utf8_unicode_ci`, ADD password_requested_at DATETIME DEFAULT \'NULL\', CHANGE list_id list_id INT DEFAULT NULL, CHANGE username username VARCHAR(180) CHARACTER SET utf8 NOT NULL COLLATE `utf8_unicode_ci`, CHANGE last_login last_login DATETIME DEFAULT \'NULL\'');
--- migrations/Version20200814082529.php
+++ migrations/Version20200814082529.php
@@ -9,17 +9,21 @@ use Doctrine\Migrations\AbstractMigration;
 
 final class Version20200814082529 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return 'Drop old migration table';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
+        $this->skipIf(
+            !$schema->hasTable('migration_versions'),
+            'Table migration_versions already removed, skipping migration'
+        );
         $this->addSql('DROP TABLE migration_versions');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         $this->addSql('CREATE TABLE migration_versions (version VARCHAR(14) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, executed_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(version)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
     }
